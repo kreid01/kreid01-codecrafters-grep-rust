52e7f5130a8a2e56bddd870b19d807a734eaeed5
Kieran Reid <kieranreid2612@gmail.com>
2025-11-30 14:43:58 +0000

    finished stage for matching on or more times by using peekable and enumerating chars

--- src/main.rs
+++ src/main.rs
@@ -2,78 +2,8 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    let mut pattern = pattern.to_string();
-    let anchor = pattern.starts_with("^");
-    if anchor {
-        pattern.remove(0);
-    }
-    let end_anchor = pattern.ends_with("$");
-    if end_anchor {
-        pattern.remove(pattern.len() - 1);
-    }
-
-    let mut temp_pattern = pattern.to_string();
-    println!("{}", end_anchor);
-
-    for (i, c) in input_line.chars().enumerate() {
-        println!("{}, {}, len{}", temp_pattern, i, input_line.len());
-        if temp_pattern.starts_with("\\d") && digit(&c) {
-            temp_pattern = temp_pattern.replacen("\\d", "", 1);
-        } else if temp_pattern.starts_with("\\w") && word_characters(&c) {
-            temp_pattern = temp_pattern.replacen("\\w", "", 1);
-        } else if temp_pattern.starts_with(&c.to_string()) {
-            temp_pattern = temp_pattern.replacen(&c.to_string(), "", 1);
-        } else {
-            if anchor {
-                return false;
-            }
-            temp_pattern = pattern.to_string();
-        }
-
-        if temp_pattern.is_empty() && end_anchor && i == input_line.len() - 1 {
-            return true;
-        }
-
-        if temp_pattern.is_empty() && !end_anchor {
-            return true;
-        }
-    }
-
-    false
-}
-
-fn digit(char: &char) -> bool {
-    char.is_numeric()
-}
-
-fn word_characters(char: &char) -> bool {
-    char.is_ascii_alphanumeric() || char == &'_'
-}
-
-fn positive_character_group(input_line: &str, pattern: &str) -> bool {
-    let pattern: Vec<String> = pattern
-        .replace("[", "")
-        .replace("]", "")
-        .split("")
-        .map(|x| x.to_string())
-        .collect();
-
-    input_line.chars().any(|x| pattern.contains(&x.to_string()))
-}
-
-fn negative_character_group(input_line: &str, pattern: &str) -> bool {
-    let pattern: Vec<String> = pattern
-        .replace("[^", "")
-        .replace("]", "")
-        .split("")
-        .map(|x| x.to_string())
-        .collect();
-
-    input_line
-        .chars()
-        .any(|x| !pattern.contains(&x.to_string()))
-}
+mod character_groups;
+mod character_matcher;
 
 fn main() {
     eprintln!("Logs from your program will appear here!");
@@ -87,22 +17,9 @@ fn main() {
     let mut input_line = String::new();
 
     io::stdin().read_line(&mut input_line).unwrap();
+    character_groups::check_character_groups(&input_line, &pattern);
 
-    if pattern.starts_with("[^")
-        && pattern.ends_with("]")
-        && negative_character_group(&input_line, &pattern)
-    {
-        process::exit(0)
-    }
-    if pattern.starts_with("[")
-        && !pattern.starts_with("[^")
-        && pattern.ends_with("]")
-        && positive_character_group(&input_line, &pattern)
-    {
-        process::exit(0)
-    }
-
-    if match_pattern(&input_line, &pattern) {
+    if character_matcher::match_pattern(&input_line, &pattern) {
         process::exit(0)
     } else {
         process::exit(1)
